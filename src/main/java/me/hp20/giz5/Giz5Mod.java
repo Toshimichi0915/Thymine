8cc8dbc94d01393147bbfd0e09fd9a6191fac3bf
Toshimichi0915 <main@toshimichi.net>
2020-08-01 04:03:09 +0900

    Fixed MC-145748

---
+++ src/main/java/me/hp20/giz5/Giz5Mod.java
@@ -0,0 +1,9 @@
+package me.hp20.giz5;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Giz5Mod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/me/hp20/giz5/mixin/SliderWidgetMixin.java
@@ -0,0 +1,38 @@
+package me.hp20.giz5.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.widget.AbstractButtonWidget;
+import net.minecraft.client.gui.widget.SliderWidget;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(SliderWidget.class)
+public class SliderWidgetMixin extends AbstractButtonWidget {
+
+    private boolean isClicked;
+
+    public SliderWidgetMixin(int x, int y, int width, int height, Text message) {
+        super(x, y, width, height, message);
+    }
+
+    @Inject(at = @At("HEAD"), method = "onClick(DD)V")
+    public void onClick(CallbackInfo info) {
+        isClicked = true;
+    }
+
+    /**
+     * @author hp20
+     * @reason fixes buttons ringing sound even when they were not clicked
+     */
+    @Overwrite
+    public void onRelease(double mouseX, double mouseY) {
+        if (isClicked) {
+            super.playDownSound(MinecraftClient.getInstance().getSoundManager());
+            isClicked = false;
+        }
+    }
+}
