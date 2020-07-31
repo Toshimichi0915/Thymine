f7349ab660118cd05333140d0005e71a7d2fc07c
Toshimichi0915 <main@toshimichi.net>
2020-08-01 08:34:51 +0900

    Added Fullbright

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ This mod requires Minecraft 1.16+, fabric loader and fabric api
 
 ### Features
 1. Removes shift animation
+2. Fullbright
 
 ### Fixed bugs
 1. Fixed [MC-145748](https://bugs.mojang.com/browse/MC-145748)
---
+++ src/main/java/me/hp20/giz5/mixin/LivingEntityMixin.java
@@ -0,0 +1,30 @@
+package me.hp20.giz5.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntity.class)
+public class LivingEntityMixin {
+
+    @Inject(at = @At("HEAD"), method = "hasStatusEffect(Lnet/minecraft/entity/effect/StatusEffect;)Z", cancellable = true)
+    public void hasStatusEffect(StatusEffect effect, CallbackInfoReturnable<Boolean> info) {
+        LivingEntity player = MinecraftClient.getInstance().player;
+        if (player != null && player.equals(this) && effect == StatusEffects.NIGHT_VISION)
+            info.setReturnValue(true);
+    }
+
+    @Inject(at = @At("HEAD"), method = "getStatusEffect(Lnet/minecraft/entity/effect/StatusEffect;)Lnet/minecraft/entity/effect/StatusEffectInstance;", cancellable = true)
+    public void getStatusEffect(StatusEffect effect, CallbackInfoReturnable<StatusEffectInstance> info) {
+        LivingEntity player = MinecraftClient.getInstance().player;
+        if (player != null && player.equals(this) && effect == StatusEffects.NIGHT_VISION)
+            info.setReturnValue(new StatusEffectInstance(StatusEffects.NIGHT_VISION, Integer.MAX_VALUE));
+    }
+
+}
