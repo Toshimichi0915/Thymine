5b2dcab83a6e03ad28fa2e307fb9babf21bb44c7
Toshimichi0915 <main@toshimichi.net>
2020-11-05 12:39:30 +0900

    Fix ToggleSprint behavior

--- src/main/java/me/hp20/giz5/SprintClientTickCallback.java
+++ src/main/java/me/hp20/giz5/SprintClientTickCallback.java
@@ -11,16 +11,13 @@ public class SprintClientTickCallback implements ClientTickCallback {
     @Override
     public void tick(MinecraftClient client) {
         Giz5Options opt = Giz5Mod.getOptions();
-        if (opt.toggleSprint) {
-            boolean pressed = Giz5Mod.getSprintKeyBinding().isPressed();
-            if (pressed && !lastPressed) {
-                opt.sprint = !opt.sprint;
-            }
-            lastPressed = pressed;
-        } else {
-            opt.sprint = false;
+        if (!opt.toggleSprint) return;
+        boolean pressed = Giz5Mod.getSprintKeyBinding().isPressed();
+        if (pressed && !lastPressed) {
+            opt.sprint = !opt.sprint;
         }
-
-        client.options.keySprint.setPressed(opt.sprint);
+        lastPressed = pressed;
+        if (opt.sprint)
+            client.options.keySprint.setPressed(true);
     }
 }
