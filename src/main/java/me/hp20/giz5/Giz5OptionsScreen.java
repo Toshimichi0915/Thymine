ebf0b1170731de45d637714536715d9ad85b1baf
Toshimichi0915 <main@toshimichi.net>
2021-01-26 16:06:49 +0900

    Change settings save timing

--- src/main/java/me/hp20/giz5/Giz5OptionsScreen.java
+++ src/main/java/me/hp20/giz5/Giz5OptionsScreen.java
@@ -54,9 +54,13 @@ public class Giz5OptionsScreen extends GameOptionsScreen {
         }
 
         this.addButton(new ButtonWidget(this.width / 2 - 100, this.height / 6 + 24 * (count + 1) / 2, 200, 20, ScreenTexts.DONE, (buttonWidget) -> {
-            Giz5Mod.saveOptions();
             this.client.openScreen(this.parent);
         }));
+    }
+
+    @Override
+    public void onClose() {
+        Giz5Mod.saveOptions();
     }
 
     @Override
