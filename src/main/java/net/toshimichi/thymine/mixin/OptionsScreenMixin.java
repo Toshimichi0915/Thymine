7393ebb839ebd9c0a048978851cfffaeede85f1e
Toshimichi0915 <main@toshimichi.net>
2022-12-28 22:10:50 +0900

    Update Mixin

--- src/main/java/net/toshimichi/thymine/mixin/OptionsScreenMixin.java
+++ src/main/java/net/toshimichi/thymine/mixin/OptionsScreenMixin.java
@@ -11,9 +11,7 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.function.Supplier;
 
@@ -24,8 +22,6 @@ public abstract class OptionsScreenMixin extends Screen {
     @Shadow
     private GameOptions settings;
 
-    private GridWidget.Adder adder;
-
     @Shadow
     protected abstract ButtonWidget createButton(Text message, Supplier<Screen> screenSupplier);
 
@@ -33,14 +29,9 @@ public abstract class OptionsScreenMixin extends Screen {
         super(title);
     }
 
-    @ModifyVariable(at = @At(value = "STORE", target = "Lnet/minecraft/client/gui/widget/GridWidget;createAdder(I)Lnet/minecraft/client/gui/widget/GridWidget$Adder;"), method = "init()V")
+    @ModifyVariable(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/GridWidget$Adder;add(Lnet/minecraft/client/gui/widget/ClickableWidget;ILnet/minecraft/client/gui/widget/Positioner;)Lnet/minecraft/client/gui/widget/ClickableWidget;", ordinal = 0), method = "init()V")
     public GridWidget.Adder createAdder(GridWidget.Adder adder) {
-        this.adder = adder;
-        return adder;
-    }
-
-    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/GridWidget$Adder;add(Lnet/minecraft/client/gui/widget/ClickableWidget;ILnet/minecraft/client/gui/widget/Positioner;)Lnet/minecraft/client/gui/widget/ClickableWidget;", ordinal = 0), method = "init()V")
-    public void init(CallbackInfo ci) {
         adder.add(createButton(Text.translatable("thymine.options.title"), () -> new ThymineOptionsScreen(this, settings)));
+        return adder;
     }
 }
