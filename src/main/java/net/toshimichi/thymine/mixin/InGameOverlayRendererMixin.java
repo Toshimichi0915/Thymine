ecca43e76c5f811c0e6eb27e8e584c3deb52a404
Toshimichi0915 <main@toshimichi.net>
2025-01-31 08:42:54 +0900

    Fix low fire

--- src/main/java/net/toshimichi/thymine/mixin/InGameOverlayRendererMixin.java
+++ src/main/java/net/toshimichi/thymine/mixin/InGameOverlayRendererMixin.java
@@ -1,7 +1,6 @@
 package net.toshimichi.thymine.mixin;
 
 import net.minecraft.client.gui.hud.InGameOverlayRenderer;
-import net.minecraft.client.render.BufferBuilder;
 import net.minecraft.client.render.VertexConsumer;
 import net.toshimichi.thymine.ThymineMod;
 import org.joml.Matrix4f;
@@ -12,9 +11,9 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(InGameOverlayRenderer.class)
 public class InGameOverlayRendererMixin {
 
-    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/BufferBuilder;vertex(Lorg/joml/Matrix4f;FFF)Lnet/minecraft/client/render/VertexConsumer;"), method = "renderFireOverlay")
-    private static VertexConsumer vertex(BufferBuilder bufferBuilder, Matrix4f matrix4f, float x, float y, float z) {
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/VertexConsumer;vertex(Lorg/joml/Matrix4f;FFF)Lnet/minecraft/client/render/VertexConsumer;"), method = "renderFireOverlay")
+    private static VertexConsumer vertex(VertexConsumer instance, Matrix4f matrix, float x, float y, float z) {
         float modifier = (float) -ThymineMod.getOptions().lowFire / 2;
-        return bufferBuilder.vertex(matrix4f, x, y + modifier, z);
+        return instance.vertex(matrix, x, y + modifier, z);
     }
 }
